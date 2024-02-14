03e3d3a058afc9458aae36b3f0f3a1d9bc9c43f7
Siddharth Kumar <siddharth.kumar28717@gmail.com>
2024-02-14 18:08:10 +0530

    minor changes

--- frontend/src/components/LeftSide.js
+++ frontend/src/components/LeftSide.js
@@ -9,7 +9,11 @@ const LeftSide = ({ data, show }) => {
     const user = useSelector(state => state.user)
     const [showNoti, setShow] = useState(false)
     // console.log(cartItems)
+    const handleClick = () => {
+        localStorage.removeItem('User');
+        window.location.reload();
 
+    }
 
     return (
         <div className='leftside'>
@@ -18,7 +22,21 @@ const LeftSide = ({ data, show }) => {
                 {show ? null : (<div className="user-info">
                     {
                         user?.user ? (
-                            <div className='user-profile-icon'>{user.user.name.charAt(0)}</div>
+                            <div style={{ display: "flex" }}>
+
+
+
+
+                                <div className='user-profile-icon'>
+                                    {user.user.name.charAt(0)}
+                                </div>
+
+                                <div >
+
+                                    <button onClick={handleClick} >Logout</button>
+                                </div>
+
+                            </div>
                         ) : (
                             <Link to="/signin">
                                 <button>Login</button>
@@ -30,7 +48,7 @@ const LeftSide = ({ data, show }) => {
                     <span>{cartItems ? cartItems?.length : 0}</span>
                     <BsCart3 />
                 </div></Link>)}
-              
+
                 {showNoti && (<div className='waring-noti'>
                     {
                         data?.map((item, i) => {
@@ -43,23 +61,25 @@ const LeftSide = ({ data, show }) => {
                 </div>)}
             </div>
 
-            {show ? null : (<div className="side-cart-area">
-                <div className="text">
-                    <h4>Order Menu</h4>
-                    <Link to='/cart'><p>Veiw All <BsFillArrowRightSquareFill /></p></Link>
-                </div>
-                <div className='cart-area'>
-                    <div className="all-items side-cart">
-                        {cartItems.slice(0, 3).map((item) => (
-                            <CartItemCard key={item.product} item={item} />
-                        ))}
-                        {cartItems.length > 0 && <Link to="/cart"><button>PROCEED TO CHECKOUT</button></Link>}
+            {
+                show ? null : (<div className="side-cart-area">
+                    <div className="text">
+                        <h4>Order Menu</h4>
+                        <Link to='/cart'><p>View All <BsFillArrowRightSquareFill /></p></Link>
                     </div>
-                </div>
-            </div>)}
+                    <div className='cart-area'>
+                        <div className="all-items side-cart">
+                            {cartItems.slice(0, 3).map((item) => (
+                                <CartItemCard key={item.product} item={item} />
+                            ))}
+                            {cartItems.length > 0 && <Link to="/cart"><button>PROCEED TO CHECKOUT</button></Link>}
+                        </div>
+                    </div>
+                </div>)
+            }
 
 
-        </div>
+        </div >
     )
 }
 
--- frontend/src/pages/Shipping.js
+++ frontend/src/pages/Shipping.js
@@ -1,110 +1,110 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import {RiAddFill} from 'react-icons/ri'
-import {useLocation,useNavigate} from 'react-router-dom'
+import { RiAddFill } from 'react-icons/ri'
+import { useLocation, useNavigate } from 'react-router-dom'
 import AddressModal from '../components/AddressModal'
 import '../styles/shipping.css'
-import {  getAdress,selectAddress } from '../actions/address';
+import { getAdress, selectAddress } from '../actions/address';
 const Shipping = () => {
 
-    const cartItems = useSelector(state=>state.cart.cartItems)
-    const allAdress = useSelector(state=>state.address.addressItems)
+    const cartItems = useSelector(state => state.cart.cartItems)
+    const allAdress = useSelector(state => state.address.addressItems)
     const dispatch = useDispatch()
-    const user = useSelector(state=>state.user.user)
-    const cartPrice=cartItems.reduce((total,itm)=>total+itm?.price*itm?.qty,0)
-    const deleviryPrice = (cartPrice>500||cartPrice===0)?0:50
+    const user = useSelector(state => state.user.user)
+    const cartPrice = cartItems.reduce((total, itm) => total + itm?.price * itm?.qty, 0)
+    const deleviryPrice = (cartPrice > 500 || cartPrice === 0) ? 0 : 50
     const discount = 0;
-    const totalPrice= (cartPrice+deleviryPrice)-discount;
+    const totalPrice = (cartPrice + deleviryPrice) - discount;
     // const auth = useSelector(state=>state.user.user)
     const location = useLocation();
     const path = location.pathname
     const navigate = useNavigate()
-    const [show,setShow]=useState(false)
-    const [selected,setSelected]=useState(0)
-   const [addressToEdit,setAddressToEdit]=useState()
-    useEffect(()=>{
-        if(!user){
+    const [show, setShow] = useState(false)
+    const [selected, setSelected] = useState(0)
+    const [addressToEdit, setAddressToEdit] = useState()
+    useEffect(() => {
+        if (!user) {
             navigate('/signin')
         }
-    },[])
-    
-    
-    useEffect(()=>{
-        if(user){
+    }, [])
+
+
+    useEffect(() => {
+        if (user) {
             dispatch(getAdress(user._id))
         }
-    },[])
-  const proceedToPayment =()=>{
-    navigate('/payment')
-  }
+    }, [])
+    const proceedToPayment = () => {
+        navigate('/payment')
+    }
 
-//   const delteAddress=(id)=>{
-//       dispatch(deleteAddress(id))
-//   }
-  const handleUpdateAddress=(address)=>{
-     setAddressToEdit(address)
-     setShow(true)
-  }
-  
-  const handleSelectAddress=(address,i)=>{
-  setSelected(i)
-  dispatch(selectAddress(address))
-  }
-    
-  return (
-    <>
-  <div className='shipping'>
-   <div className="shipping-details">
-       <div className="address">
-        <h3>Select Delivery Address</h3>
-        <div className="add-sec-area">
-            {
-            allAdress.length>0?(allAdress.map((address,i)=>(
-                <div onClick={()=>handleSelectAddress(address,i)} className={`og-add ${selected===i&& 'selected'}`} key={address._id}>
-                <p>{address.name}</p>
-                <span>{address.address},{address.town}</span>
-                <span>{address.city},{address.state} -{address.pinCode} </span>
-                <span><b>Mobile No:</b>{address.mobNo}</span>
-                
-               </div>
-            ))
-            ):<h3 style={{padding:'20px 0'}}>No Address found! Add one</h3>}
-           
-            <div className="add-address" onClick={()=>setShow(true)}>
-             <div className="add">
-                 <RiAddFill/>
-                 <p>Add Address</p>
-             </div>
+    //   const delteAddress=(id)=>{
+    //       dispatch(deleteAddress(id))
+    //   }
+    const handleUpdateAddress = (address) => {
+        setAddressToEdit(address)
+        setShow(true)
+    }
+
+    const handleSelectAddress = (address, i) => {
+        setSelected(i)
+        dispatch(selectAddress(address))
+    }
+
+    return (
+        <>
+            <div className='shipping'>
+                <div className="shipping-details">
+                    <div className="address">
+                        <h3>Select Delivery Address</h3>
+                        <div className="add-sec-area">
+                            {
+                                allAdress.length > 0 ? (allAdress.map((address, i) => (
+                                    <div onClick={() => handleSelectAddress(address, i)} className={`og-add ${selected === i && 'selected'}`} key={address._id}>
+                                        <p>{address.name}</p>
+                                        <span>{address.address},{address.town}</span>
+                                        <span>{address.city},{address.state} -{address.pinCode} </span>
+                                        <span><b>Mobile No:</b>{address.mobNo}</span>
+
+                                    </div>
+                                ))
+                                ) : <h3 style={{ padding: '20px 0' }}>No Address found! Add one</h3>}
+
+                            <div className="add-address" onClick={() => setShow(true)}>
+                                <div className="add">
+                                    <RiAddFill />
+                                    <p>Add Address</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="checkout-area">
+                        <div className="billing">
+                            <h4>PRICE DETAILS</h4>
+                            <div className="details">
+                                <div className="item">
+                                    <p>Price</p>
+                                    <p><span>₹</span>{cartPrice}</p>
+                                </div>
+
+                                <div className="item">
+                                    <p>Delivery Charges</p>
+                                    <p>{deleviryPrice === 0 ? <span className='free'>Free</span> : <span>₹{deleviryPrice}</span>}</p>
+                                </div>
+                            </div>
+                            <div className="total">
+                                <h3>Total</h3>
+                                <h3><span>₹</span>{totalPrice}</h3>
+                            </div>
+                        </div>
+                        {allAdress.length > 0 ? <button onClick={proceedToPayment} disabled={totalPrice === 0 ? true : false}>PROCEED TO PAYMENT</button> : ""}
+                    </div>
+                </div>
             </div>
-        </div>
-       </div>
-       
-       <div className="checkout-area">
-                      <div className="billing">
-                        <h4>PRICE DETAILS</h4>
-                        <div className="details">
-                            <div className="item">
-                                <p>Price</p>
-                                <p><span>₹</span>{cartPrice}</p>
-                            </div>
-                            
-                            <div className="item">
-                                <p>Delivery Charges</p>
-                                <p>{deleviryPrice===0?<span className='free'>Free</span>:<span>₹{deleviryPrice}</span>}</p>
-                            </div>
-                        </div>
-                        <div className="total">
-                            <h3>Total</h3>
-                            <h3><span>₹</span>{totalPrice}</h3>
-                        </div>
-                      </div>
-                      <button onClick={proceedToPayment} disabled={totalPrice===0?true:false}>PROCEED TO PAYMENT</button>
-        </div>
-   </div>
-  </div>
-  <AddressModal show={show} setShow={setShow} addressToEdit={addressToEdit}/>
-  </>
-  );
+            <AddressModal show={show} setShow={setShow} addressToEdit={addressToEdit} />
+        </>
+    );
 };
 
 export default Shipping;
