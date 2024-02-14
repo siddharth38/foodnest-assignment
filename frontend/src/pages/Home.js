bf1c65d39f9f55683189dc9f05c3be2c5e60b820
Siddharth Kumar <siddharth.kumar28717@gmail.com>
2024-02-14 18:21:37 +0530

    minor changes

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import LeftSide from '../components/LeftSide'
 import MainArea from '../components/MainArea'
 
 const Home = () => {
-    return (
-        <>
-          <MainArea/>
-          <LeftSide/>  
-        </>
-    )
+  useEffect(() => {
+    localStorage.removeItem('rzp_device_id');
+    localStorage.removeItem('rzp_checkout_anon_id');
+  }, [])
+  return (
+    <>
+      <MainArea />
+      <LeftSide />
+    </>
+  )
 }
 
 export default Home
